8dd671262b6cb91700d90d5cb0f94765a63b9bd3
changi123 <133079671+changi123@users.noreply.github.com>
2023-06-28 07:59:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] numbers) {
+		int answer = 0;
+		
+		
+		for( int i= 0; i<10;i++) {
+			answer+=i;
+		}
+		
+		for(int i= 0; i< numbers.length;i++) {
+			answer=answer - numbers[i];
+		}
+		
+		
+		
+
+		
+		return answer;
+    }
+}
