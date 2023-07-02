0dd57373555ee0d75f5817264e2d1fb671c268e5
changi123 <133079671+changi123@users.noreply.github.com>
2023-07-03 08:47:39 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.65 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+                String [] arr1_sr  = new String [n];
+        String [] arr2_sr = new String [n];
+       
+        String s = "";
+        
+        for(int i= 0; i<n;i++) {
+        	 s =Integer.toBinaryString(arr1[i]);
+        	 int len = s.length();
+             if(n == s.length()) {
+            	 arr1_sr[i] = s ;
+            	 continue;
+             }
+        	 for(int  j= n; j>len ; j--) {
+        	
+        		 s = "0" +s;
+        		 
+        		 	
+        	 }
+        	 arr1_sr[i] = s;
+        			 
+        }
+        
+        for(int i =0; i< n  ; i++ ) {
+        	 s= Integer.toBinaryString(arr2[i]);
+        	 int len = s.length();
+        	 if(n==s.length()) {
+        		 arr2_sr[i] = s;
+        		 continue;
+        	 }
+        	 for( int j = n; j > len;j--) {
+        		 
+        		 s = "0" + s;
+        		 
+        	 }
+        	 arr2_sr[i] = s;
+        }
+        
+        String[] answer = new String [n];
+        
+       
+        ArrayList list = new ArrayList();
+        for(int i= 0; i<n;i++) {
+        	list.add("");
+        }
+        
+        
+        for(int i= 0; i<list.size();i++) {
+        	answer[i] = "";
+        }
+        
+        
+       
+        
+        
+        for(int i= 0; i<arr1_sr.length;i++) {
+        	
+        	for(int j= 0; j <arr2_sr.length;j++)
+        	if(arr1_sr[i].charAt(j) == '0' && arr2_sr[i].charAt(j) == '0') {
+        		
+        		answer[i] += " ";
+        	} else {
+        		answer[i] += "#";
+        	}
+        }
+        
+        return answer;
+    }
+}
