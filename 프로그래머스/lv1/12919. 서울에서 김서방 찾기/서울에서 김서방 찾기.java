a6377097d6d299d9a18e3fda8e0612516f1c632e
changi123 <133079671+changi123@users.noreply.github.com>
2023-07-28 14:54:13 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.14 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,21 +1,15 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public String solution(String[] seoul) {
-              String answer = "";
-        
-        
-        ArrayList list = new ArrayList();
-        
+               String answer = "김서방은 x에 있다";
+        String cnt = "";
         for(int i= 0; i<seoul.length;i++) {
-        	list.add(seoul[i]);
-        	
+        	if(seoul[i].equals("Kim")) {
+        		cnt = Integer.toString(i); 
+        		break;
+        	}
         }
         
-        int idx = list.indexOf("Kim");
-        
-        answer = String.format("김서방은 %d에 있다",idx);
+        answer = answer.replace("x", cnt);
         
         
         return answer;
