2929b6925e3fbacad0304dbac4b4215679af4965
changi123 <133079671+changi123@users.noreply.github.com>
2023-07-28 14:30:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,18 +1,43 @@
 class Solution {
-public int solution(int num) {
-        int answer = 0;
-        if( num == 1 ) return 0;
-        long n = num;
-        while( n != 1  && answer < 500 ) {            
-           if( n % 2 == 0) { // 짝수
-              n /= 2;              
-           }else {            // 홀수
-              n *=3;            n+= 1;
-           }
-           answer++; 
-           
-        } // while
-         
-        return n==1 ? answer : -1;
+    public int solution(int num) {
+			int answer = 0;
+		if(num == 1 ) {
+			
+			return answer;
+		}
+		String s = Integer.toString(num);
+
+		int i = 0 ;
+		
+		long longNum = Long.parseLong(s); 
+		while(true) {
+			if(i == 500) {
+				answer = -1;
+				break;
+			}
+			if( longNum  % 2  ==0) {
+				longNum = longNum / 2;
+				i++;
+				answer ++;
+				if(longNum == 1) {
+					break;
+				}
+
+			} else {
+				longNum = longNum * 3 +1;
+				i++;
+				answer ++;
+				if(longNum == 1) {
+					break;
+				}
+
+
+			}
+			
+			
+
+		}
+
+		return answer;
     }
 }
