5a99c2b31533079e1a363cd0fc197aaf00628ed9
changi123 <133079671+changi123@users.noreply.github.com>
2023-07-28 14:12:52 +0900

    [level 1] Title: 하샤드 수, Time: 0.17 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean solution(int x) {
-           boolean answer = false;
+        boolean answer = true;
         
         String s = Integer.toString(x);
         
         String [] sr = s.split("");
+        
         int sum = 0;
-        int i = 0;
-        while(i<sr.length) {
-        	sum += Integer.parseInt(sr[i]); 
-        	i++;
+        
+        for(int i = 0; i<sr.length;i++) {
+        	
+        	sum =sum + Integer.parseInt(sr[i]);
+        	
         }
         
-        if(x%sum == 0) {
-        	return answer = true;
-        } else {
-        	return answer ;
+        if(x % sum  != 0) {
+        	
+        	answer = false;
         }
+        
+        return answer;
     }
 }
