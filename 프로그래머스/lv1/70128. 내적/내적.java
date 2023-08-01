a9a57bfaaab518f535323dd26bc799538bdea261
changi123 <133079671+changi123@users.noreply.github.com>
2023-08-01 14:56:40 +0900

    [level 1] Title: 내적, Time: 0.03 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int[] a, int[] b) {
-       	int [] m = new int [a.length];
-    	
-    	for(int i= 0; i<a.length;i++) {
-    		m[i] = a[i]*b[i];
-    	}
-    	
-    	int answer =0;
-    	for(int i= 0; i<m.length;i++) {
-    		answer+=m[i];
-    	}
-    	
+        int answer = 0;
+        
+        for(int i= 0; i< a.length;i++) {
+        	answer =  answer + a[i] * b[i] ;
+        }
+        
         return answer;
     }
 }
