8dd1e42e7a01de39474a4b39e10708e0e3160d84
changi123 <133079671+changi123@users.noreply.github.com>
2023-08-01 15:21:36 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 11.87 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,24 +1,41 @@
 import java.util.*;
 import java.io.*;
-
 class Solution {
     public String solution(String s) {
-              String answer = "";
-        String [] sr = s.split("");
-        ArrayList<String> list = new ArrayList<String>();
+       String answer = "";
         
-        for(int i= 0; i< sr.length;i++) {
-        	list.add(sr[i]);
+        //대문자
+        ArrayList list1  = new ArrayList();
+        
+        
+        //소문자 
+        ArrayList list2  = new ArrayList();
+        
+        
+        for(int i= 0; i<s.length();i++) {
+        	
+        	if( Character.isUpperCase(s.charAt(i))) {
+        		list1.add(s.charAt(i));	
+        	} else {
+        		list2.add(s.charAt(i));		
+        	}
+        	
         }
         
-        Collections.sort(list, Collections.reverseOrder());
+        list1.sort(Collections.reverseOrder());
+        list2.sort(Collections.reverseOrder());
         
-       for(int i= 0; i<sr.length;i++) {
-    	   sr[i] = list.get(i);
-    	   answer+=sr[i];
-       }
-       
-       
+        String a = "" ;
+        String b = "" ;
+        
+        for(int i= 0; i<list1.size();i++) {
+        	a = a + list1.get(i);
+        }
+        for(int i= 0; i<list2.size();i++) {
+        	b = b + list2.get(i);
+        }
+        
+        answer = answer + b +a;
         
         return answer;
     }
