27ffc2d55e18adb194d2b0830ef474f06d53fc2a
changi123 <133079671+changi123@users.noreply.github.com>
2023-08-17 15:24:42 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.15 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+         String answer = "";
+        
+        String first = "";
+        
+        List list = new ArrayList();
+        
+        for(int i= 1; i < food.length;i++) {
+        	
+        	int com = 0;
+        	if( food[i] % 2 ==1 ) {
+        		com = food[i] -1;
+        	} else {
+        		com = food[i];
+        	}
+        	for(int j = 0 ; j < com /2 ; j++) {
+        		first = first + Integer.toString(i);
+        		list.add(Integer.toString(i));
+        	}
+        	if(i == food.length-1) {
+        		
+        		first = first + "0";        
+        	}	
+        }
+        Collections.reverse(list);
+        
+        for(int i= 0; i< list.size(); i++) {
+        	first = first + (String) list.get(i);
+        }
+        return first;
+    }
+}
