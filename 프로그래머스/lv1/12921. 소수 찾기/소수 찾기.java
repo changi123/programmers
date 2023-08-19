b7d0843ee2bf3c5ed0c3bf87cc762d89430b08f9
changi123 <133079671+changi123@users.noreply.github.com>
2023-08-19 16:21:42 +0900

    [level 1] Title: 소수 찾기, Time: 203.71 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,22 +1,18 @@
 class Solution {
     public int solution(int n) {
-              int answer = 0;
-        int [] arr = new int [n+1];
-        for( int i = 2 ; i<= n ; i++) {
-        	arr[i] = i ;
-        }
+         int answer = 0;
         
-        for(int i = 2; i<= n  ; i++) {
-        	if(arr[i]== 0 ) {
-        		continue;
+        for(int i= 2; i<=n ;i++) {
+        	int cnt = 0;
+        	for(int j = 1 ; j<=Math.sqrt(i) ; j++) {
+        		if( i  % j == 0) {
+        			cnt++;
+        		}
+        		if(cnt >1 ) {
+        			break;
+        		}
         	}
-        	for(int j = i* 2 ; j<=n ; j+=i) {
-        		arr[j] = 0 ;
-        	}
-        }
-        
-        for(int i = 2 ; i<=n ;i++) {
-        	if( arr[i] != 0 ) {
+        	if(cnt == 1) {
         		answer++;
         	}
         }
