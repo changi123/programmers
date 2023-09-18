fe7c9563b0b593804668acf71d89a8851f5a49ab
changi123 <133079671+changi123@users.noreply.github.com>
2023-09-18 12:36:28 +0900

    [unrated] Title: 둘만의 암호, Time: 1.54 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, String skip, int index) {
+		String answer = "";
+		char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		
+		
+		List<Character> list = new ArrayList<Character>();
+		
+		for(char c :alphabet) {
+			list.add(c);
+		}
+			
+		for(int i= 0; i<skip.length();i++) {
+			list.remove(list.indexOf(skip.charAt(i)));
+		}
+		
+//		System.out.println(list.size());
+		
+		
+		for(int i = 0 ; i<s.length();i++) {
+//			int a = list.indexOf(s.charAt(i))+index;
+//			if(list.indexOf(s.charAt(i))+index<list.size() ) {
+//				answer+= list.get(  list.indexOf(s.charAt(i))+index ) ;
+//			} else {
+//				answer+= list.get(list.indexOf(s.charAt(i))+index - list.size());
+//			}
+			
+			int a = list.indexOf(s.charAt(i)) + index;
+			if (a >= list.size()) {
+			    a %= list.size();
+			}
+			answer += list.get(a);
+
+		}
+		
+		return answer;
+    }
+}
