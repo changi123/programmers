24cfb8e92779e7551109ee20c669ab33d7862823
changi123 <133079671+changi123@users.noreply.github.com>
2023-10-05 15:45:42 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 5.40 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] board, int[] moves) {
-  int answer = 0;
+         int answer = 0;
         
         
 //        for(int i= 0; i  < board.length;i++) {
@@ -14,10 +14,37 @@ class Solution {
         
         
         List<Integer> list = new ArrayList<Integer>();
+        Stack<Integer> st = new Stack<Integer>();
 //        List<Integer> list2 = new ArrayList<Integer>();
         
-        
-//        int index = 0 ;
+
+//        for(int i= 0; i <moves.length;i++) {
+//        	
+//        	int 뽑기 = moves[i]-1;
+//        	
+//        	뽑기 :for(int j = 0; j <board.length;j++) {
+//        		
+//        		for(int k = 0 ; k <board[j].length;k++) {
+//        
+//        			if(k == 뽑기 && board[j][k] !=0) {
+//        				list.add(board[j][k]);
+//
+//        				if(list.size()>=2 && list.get(list.size()-1) == list.get(list.size()-2)) {
+//        					answer++;
+//        					
+//        					list.remove(list.size()-1);
+//        					list.remove(list.size()-1);  					
+//        				}
+////        				list2.add(board[j][k]);
+//        				board[j][k] = 0 ;
+//        				break 뽑기;
+//        			}
+//        		}
+//        
+//     
+//        	}
+//        	
+//        }
         for(int i= 0; i <moves.length;i++) {
         	
         	int 뽑기 = moves[i]-1;
@@ -28,11 +55,14 @@ class Solution {
         
         			if(k == 뽑기 && board[j][k] !=0) {
         				list.add(board[j][k]);
-        			
-        				if(list.size()>=2 && list.get(list.size()-1) == list.get(list.size()-2)) {
+        				st.add(board[j][k]);
+        				if(st.size()>=2 && st.get(st.size()-1) == st.get(st.size()-2)) {
         					answer++;
-        					list.remove(list.size()-1);
-        					list.remove(list.size()-1);  					
+        					
+//        					list.remove(list.size()-1);
+//        					list.remove(list.size()-1);
+        					st.pop();
+        					st.pop();
         				}
 //        				list2.add(board[j][k]);
         				board[j][k] = 0 ;
@@ -44,6 +74,8 @@ class Solution {
         	}
         	
         }
+        
+        
         
         
 //      for(int i= 0; i  < board.length;i++) {
