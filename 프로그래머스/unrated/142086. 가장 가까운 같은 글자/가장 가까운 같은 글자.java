8a9b1495d94fbf7e039ea2cf5b03b8f55bbaf239
changi123 <133079671+changi123@users.noreply.github.com>
2023-10-10 10:34:15 +0900

    Update 가장 가까운 같은 글자.java

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -7,7 +7,9 @@ class Solution {
     	for(int i = 0 ; i<s.length(); i++) {
     	answer[i] = -1;	
     	//char c= s.charAt(i);
-    		
+
+
+            // 기준이랑 똑같은 값 인덱스 차이 넣기
     		for(int j = 0; j <i;j++) {
     			if(s.charAt(i)==s.charAt(j)) {
     			
