07436508f2f0f53eebea2bf38c93716ac656dbc2
changi123 <133079671+changi123@users.noreply.github.com>
2023-10-10 10:33:38 +0900

    Update K번째수.java

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -7,7 +7,8 @@ class Solution {
 			int a = commands[i][0];
 			int b = commands[i][1];
 			int c = commands[i][2];
-			
+
+			// 배열 카피해서 오름차순 정렬 후 3번째 값 넣기
 			int [] arr = Arrays.copyOfRange(array, a-1, b);
 			Arrays.sort(arr);
 			answer[i] = arr[c-1];
