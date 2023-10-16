2381f2f5b35d88ad3c2f7b44025f78651c01fb37
changi123 <133079671+changi123@users.noreply.github.com>
2023-10-16 16:11:57 +0900

    [level 1] Title: 추억 점수, Time: 0.12 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,29 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-		int[] answer = new int[photo.length];
+			int[] answer = new int[photo.length];
 
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 
+		// 이름별 점수 
 		for (int i = 0; i < name.length; i++) {
 			map.put(name[i], yearning[i]);
 		}
 
-		
 		List<String> list = Arrays.asList(name);
+
+		int i = 0;
 		
-		int i = 0 ;
-		for(String [] sr :photo) {
+		// 한줄 가져오기
+		for (String[] sr : photo) {
 			List<String> names = Arrays.asList(sr);
-			int sum = 0 ;
-			for(String s :sr) {
-				if(list.contains(s)) {
-					sum+=map.get(s);
+			int sum = 0;
+			// 한줄에서 이름 하나씩 꺼내서 name에 있다면
+			// 그 이름에 해당하는 만큼 sum 중첩
+			for (String s : sr) {
+				if (list.contains(s)) {
+					sum += map.get(s);
 				}
 			}
 			answer[i] = sum;
 			i++;
-			
+
 		}
 		return answer;
     }
