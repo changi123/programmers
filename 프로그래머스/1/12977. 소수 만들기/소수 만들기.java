2d2c93f8abde6725588918437698fb3aa5e9ff71
changi123 <133079671+changi123@users.noreply.github.com>
2023-10-19 15:26:25 +0900

    Update 소수 만들기.java

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,34 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-     int answer = 0;
-        
-        List list = new ArrayList();
-        
-        for(int i= 0;  i< nums.length;i++) {
-        	
-        	for(int j = i+1; j < nums.length;j++) {
-        		
-        		for(int k = j+1 ; k<nums.length ; k++) {
-        			int sum = 0 ; 
-        			sum = nums[i] + nums[j] + nums[k];	
-        			list.add(sum);
-        		}
-        	}
-        }
-        
-        for(int i =0; i <list.size();i++) {
-        	int cnt = 0 ;
-        	for(int j = 1 ; j<=(int) list.get(i) ; j++) {
-        		if( (int) list.get(i) % j ==0 ) {
-        			cnt++;
-        		}
-        	}
-        	if(cnt == 2 ) {
-        		answer ++;
-        	}
-        }
-        
-        return answer;
+    	int answer = 0;
+		List<Integer> list = new ArrayList<>();
+		// 세 개 더해서 나올 수 있는 모든 합 리스트에 박자
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				for (int k = j + 1; k < nums.length; k++) {
+					int sum = 0;
+					sum = nums[i] + nums[j] + nums[k];
+					list.add(sum);
+				}
+			}
+		}
+		
+		// 박은 리스트에서 소수 판별
+		for (int i = 0; i < list.size(); i++) {
+			int cnt = 0;
+			for (int j = 1; j <= list.get(i); j++) {
+				if (list.get(i) % j == 0) {
+					cnt++;
+				}
+			}
+			if (cnt == 2) {
+				answer++;
+			}
+		}
+
+		return answer;
     }
 }
