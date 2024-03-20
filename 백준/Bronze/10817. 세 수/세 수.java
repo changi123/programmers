9fd80530e38f553939445debc5121f0b32681d9d
changi123 <133079671+changi123@users.noreply.github.com>
2024-03-20 15:46:21 +0900

    [Bronze III] Title: 세 수, Time: 200 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+		int [] arr = new int[3];
+		for(int i= 0 ; i < arr.length;i++) {			
+			arr[i]=  Integer.parseInt(sc.next());
+		}
+		
+		
+		Arrays.sort(arr);
+		
+		
+
+
+		System.out.println(arr[1]);
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
