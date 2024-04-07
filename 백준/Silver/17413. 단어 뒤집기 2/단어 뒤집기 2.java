01438e3af67f4f9957b106af935608fd6cae3f00
changi123 <133079671+changi123@users.noreply.github.com>
2024-04-07 12:02:16 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 1308 ms, Memory: 35632 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		Stack<Character> st = new Stack();
+		StringBuilder sb = new StringBuilder();
+		boolean sbFlag = false;
+
+		char c ='a';
+		for(int i= 0 ; i < s.length(); i++) {
+			if( i != s.length()-1) {
+				c= s.charAt(i+1);
+			}
+			if( s.charAt(i) == '<') {
+				while(!st.isEmpty()) {
+					System.out.print(st.pop());
+				}
+
+				sbFlag = true;
+				sb.append(s.charAt(i));
+				continue;
+			}else if( s.charAt(i) == '>' && sbFlag ==true) {
+				sb.append(s.charAt(i));
+				System.out.print(sb.toString());
+				sb.delete(0,sb.length());
+				sbFlag = false;
+				continue;
+			} 
+
+			if( sbFlag ) {
+				sb.append(s.charAt(i));
+			} else{
+
+				st.push(s.charAt(i));
+				if( s.charAt(i) == ' ' || i == s.length()-1) {
+					while(!st.isEmpty()) {
+						if(st.peek() == ' ') {
+							st.pop();
+							continue;
+						}
+						System.out.print(st.pop());
+					}
+					System.out.print(' ');
+
+				}
+
+			} 
+
+
+
+		}
+		}
+	}
+
