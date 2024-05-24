358643819def2d2b41a9502ebbc08ab9dbf9837f
changi123 <133079671+changi123@users.noreply.github.com>
2024-05-24 13:06:22 +0900

    [Silver V] Title: 카드1, Time: 252 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+		Queue<Integer> q = new LinkedList<Integer>();
+
+
+		int n = Integer.parseInt(sc.next());
+		for(int i= 1 ; i <= n ; i ++) {
+			q.offer(i);
+		}
+
+		int i = 1 ;
+		while(q.size()!=1) {
+			System.out.print(q.poll()+ " ");
+			int num = q.poll();
+			q.add(num);
+			
+		}
+
+
+		System.out.println(q.peek());
+	}
+
+}
