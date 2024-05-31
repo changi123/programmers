182c8536da90dc0cafb62db0977ba16aef7a5a8e
changi123 <133079671+changi123@users.noreply.github.com>
2024-05-31 19:06:07 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+-- 코드를 입력하세요
+-- 2022년 10월에 작성된 게시글 제목
+-- , 게시글 ID BOARD_ID
+-- , 댓글 ID REPLY_ID
+-- , 댓글 작성자 ID WRITER_ID
+-- , 댓글 내용 CONTENTS
+-- , 댓글 작성일을 조회하는 SQL문을 작성해주세요 CREATED_DATE
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, 
+DATE_FORMAT(r.CREATED_DATE,'%Y-%m-%d')
+FROM USED_GOODS_BOARD b
+JOIN USED_GOODS_REPLY r
+ON b.BOARD_ID = r.BOARD_ID
+WHERE DATE_FORMAT(b.CREATED_DATE, '%Y-%m') = '2022-10'
+ORDER BY r.CREATED_DATE , b.TITLE ;
