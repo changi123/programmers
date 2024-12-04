b6ef52eabcccbb68998fafb71ae355bd3ec02b78
changi123 <133079671+changi123@users.noreply.github.com>
2024-12-04 15:28:17 +0900

    [Silver II] Title: 최소 힙, Time: 664 ms, Memory: 69760 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		while(n-- >  0 ) {
+			int num = Integer.parseInt(sc.next());
+			if( num !=0 ) {
+				pq.offer(num);
+			} else if ( num == 0 ) {
+				if( pq.isEmpty()) {
+					sb.append(0+" "+"\n");
+				} else {
+					sb.append(pq.poll()+ " "+ "\n");
+				}
+			}
+			
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
