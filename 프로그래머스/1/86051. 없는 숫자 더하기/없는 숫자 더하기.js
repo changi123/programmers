61933db5fddf245fbbbdf3a686c69739ccd98935
changi123 <133079671+changi123@users.noreply.github.com>
2025-02-07 23:51:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    var sum = 0 ;
+    for(var i = 0 ; i <= 9 ; i ++){
+        if( !numbers.includes(i)){
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
