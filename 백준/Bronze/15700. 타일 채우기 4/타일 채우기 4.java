103a157fcd4ca70ec435caec3768d1ce1c5d5361
changi123 <133079671+changi123@users.noreply.github.com>
2025-08-24 22:55:32 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+		Long n = Long.parseLong(sc.next());
+		Long m = Long.parseLong(sc.next());
+		
+		
+		System.out.print(n * m / 2 );
+	}
+
+}
